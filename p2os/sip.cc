f001399a1d3ada01cdc4860daf38efedee8ab335
Dave Feil-Seifer <dfseifer@usc.edu>
2009-07-06 06:34:02 +0000

    fix to comply with new TF API

--- p2os/sip.cc
+++ p2os/sip.cc
@@ -60,7 +60,7 @@ void SIP::FillStandard(ros_p2os_data_t* data)
     py += this->ypos / 1e3;
     pa = DTOR(this->angle);
   }
-	tf::PoseTFToMsg(tf::Pose(tf::Quaternion(pa,0,0),tf::Vector3(px,py,0)),data->position.pos);
+	tf::poseTFToMsg(tf::Pose(tf::Quaternion(pa,0,0),tf::Vector3(px,py,0)),data->position.pos);
 
   // add rates
   data->position.vel.vel.vx = ((lvel + rvel) / 2) / 1e3;
