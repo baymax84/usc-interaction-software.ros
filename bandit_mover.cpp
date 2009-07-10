628b30eae9515d603a3581df09b06f5c0d11c1bb
Dave Feil-Seifer <dfseifer@usc.edu>
2009-07-10 22:41:15 +0000

    bandit_mover upgrades

--- bandit_mover.cpp
+++ bandit_mover.cpp
@@ -1,19 +1,57 @@
 #include "ros/ros.h"
 #include "bandit_msgs/JointArray.h"
 
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Value_Slider.H>
+#include <stdio.h>
+
 #define DTOR( a ) a * M_PI / 180.0
+
+void slider_cb( Fl_Widget* o, void* )
+{
+  
+}
 
 int main( int argc, char* argv[] )
 {
   ros::init(argc,argv,"bandit_mover");
   ros::NodeHandle n;
-  
-  ros::Publisher joint_publisher = n.advertise<bandit_msgs::Joint>("joint_ind",1000);
+  ros::Publisher joint_publisher = n.advertise<bandit_msgs::JointArray>("joint_ind",1000);
   ros::Rate loop_rate(10);
 
   bandit_msgs::JointArray joint_array;
-
   bandit_msgs::Joint j;
+
+  int slider_w = 200;
+  int slider_h = 50;
+  int padding = 10;
+  int num_sliders = 19;
+
+  int win_w = padding*2+slider_w;
+  int win_h = padding*(1+num_sliders)+num_sliders*slider_h;
+
+  Fl_Window win( win_w, win_h, "Bandit Mover" );
+  win.begin();
+  Fl_Value_Slider* sliders[19];
+
+  for( int i = 0; i < num_sliders; i++ )
+  {
+    sliders[i] = new Fl_Value_Slider( padding, (i+1)*padding+i*slider_h,slider_w, slider_h, "slider" );
+    sliders[i]->callback( slider_cb );
+  }
+
+  win.end();
+  win.show();
+
+  while( n.ok() && Fl::check() )
+  {
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  /*
   j.id = 0;
   j.angle = 0;
 
@@ -31,6 +69,6 @@ int main( int argc, char* argv[] )
     ros::spinOnce();
     loop_rate.sleep();
   }
-
+  */
   return 0;
 }
