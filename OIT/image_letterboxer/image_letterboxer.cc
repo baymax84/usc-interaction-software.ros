e11e7f869fc99e6e06a62a558684215737c3b688
Dave Feil-Seifer <dfseifer@usc.edu>
2010-04-02 21:15:56 +0000

    working letterboxer

--- OIT/image_letterboxer/image_letterboxer.cc
+++ OIT/image_letterboxer/image_letterboxer.cc
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <cv_bridge/CvBridge.h>
 #include <opencv/cv.h>
+#include <opencv/highgui.h>
 #include <opencv/cxtypes.h>
 #include <image_transport/image_transport.h>
 
@@ -11,7 +13,22 @@ image_transport::Publisher pub_;
 sensor_msgs::ImageConstPtr last_msg_;
 sensor_msgs::CvBridge img_bridge_;
 
+ros::Publisher info_pub_;
+ros::Subscriber info_sub_;
+
 int border;
+
+void info_cb(const sensor_msgs::CameraInfoConstPtr& msg)
+{
+	sensor_msgs::CameraInfo newmsg = *msg;
+	newmsg.height += 2*border;
+	newmsg.width += 2*border;
+
+	newmsg.P[2] += border;
+	newmsg.P[5] += border;
+
+	info_pub_.publish(newmsg);
+}
 
 void image_cb(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -20,10 +37,19 @@ void image_cb(const sensor_msgs::ImageConstPtr& msg)
 
   if (img_bridge_.fromImage(*msg, "bgr8"))
   {
-    IplImage *orig;
+    IplImage *orig, *dest;
     orig = img_bridge_.toIpl();
-    cv::Mat dest(orig->height + border*2, orig->width + border*2, orig->depth);
-    cv::copyMakeBorder(orig,dest,border,border,border,border,BORDER_CONSTANT);
+		CvPoint offset; offset.x = border; offset.y = border;
+    // *dest = cvCreateMat(orig->height + border*2, orig->width + border*2, orig->depth);
+		dest = cvCreateImage(cvSize(orig->width+border*2,orig->height+border*2), 8, 3 );
+		cvZero(dest);
+    //cvCopyMakeBorder(orig,dest,border,border,border,border,IPL_BORDER_CONSTANT);
+    cvCopyMakeBorder(orig,dest,offset,IPL_BORDER_CONSTANT,cvScalar(0,0,0));
+
+		//cvShowImage("display",dest);
+		//cvWaitKey(10);
+		sensor_msgs::ImagePtr newmsg = sensor_msgs::CvBridge::cvToImgMsg(dest, "bgr8");
+		pub_.publish( newmsg );
   }
   else
     ROS_ERROR("Unable to convert %s image to bgr8", msg->encoding.c_str());
@@ -32,12 +58,14 @@ void image_cb(const sensor_msgs::ImageConstPtr& msg)
 int main( int argc, char* argv[] )
 {
   ros::init(argc, argv, "image_letterboxer" );
-  ros::NodeHandle nh("~");
+  ros::NodeHandle nh;
   nh.param("border", border, 100 );
-
+	//cvNamedWindow("display",1);
   image_transport::ImageTransport it(nh);
   sub_ = it.subscribe("image_raw", 1, &image_cb);
-  pub_ = it.advertise("letterbox/image_raw", 1); 
+  pub_ = it.advertise("letterbox/image_raw", 1);
+	info_sub_ = nh.subscribe( "camera_info", 1, &info_cb);
+	info_pub_ = nh.advertise<sensor_msgs::CameraInfo>( "letterbox/camera_info", 1 );
   ros::spin();
 
   return 0;
