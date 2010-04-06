a2aec9646fac164616be2ea6d2db0b1199ffd7d2
Dave Feil-Seifer <dfseifer@usc.edu>
2010-04-06 23:58:44 +0000

    working image_letterboxer for image_proc calibration including the distorted edges

--- OIT/image_letterboxer/image_letterboxer.cc
+++ OIT/image_letterboxer/image_letterboxer.cc
@@ -16,7 +16,12 @@ sensor_msgs::CvBridge img_bridge_;
 ros::Publisher info_pub_;
 ros::Subscriber info_sub_;
 
+int old_width = 640;
+int old_height = 480;
+
 int border;
+
+int display = 0;
 
 void info_cb(const sensor_msgs::CameraInfoConstPtr& msg)
 {
@@ -24,9 +29,30 @@ void info_cb(const sensor_msgs::CameraInfoConstPtr& msg)
 	newmsg.height += 2*border;
 	newmsg.width += 2*border;
 
-	newmsg.P[2] += border;
-	newmsg.P[5] += border;
+  
 
+  double fx = newmsg.P[0] ;//*(old_width+2*border) / old_width;
+  double fy = newmsg.P[5] ;//* (old_height+2*border) / old_height;
+
+  double cx = newmsg.P[2] + border;
+  double cy = newmsg.P[6] + border;
+
+	newmsg.P[2] = cx;
+	newmsg.P[6] = cy;
+  newmsg.K[2] = cx;
+  newmsg.K[5] = cy;
+
+	newmsg.P[0] = fx;
+	newmsg.P[5] = fy;
+  if( !display )
+  {
+    ROS_INFO( "height: %d width: %d", newmsg.width, newmsg.height );
+    for( int i = 0; i < 9; i++ )
+    {
+      ROS_INFO( "K[%d]: %f", i, newmsg.K[i]);
+    }
+    display = 1;
+  }
 	info_pub_.publish(newmsg);
 }
 
@@ -40,16 +66,12 @@ void image_cb(const sensor_msgs::ImageConstPtr& msg)
     IplImage *orig, *dest;
     orig = img_bridge_.toIpl();
 		CvPoint offset; offset.x = border; offset.y = border;
-    // *dest = cvCreateMat(orig->height + border*2, orig->width + border*2, orig->depth);
 		dest = cvCreateImage(cvSize(orig->width+border*2,orig->height+border*2), 8, 3 );
 		cvZero(dest);
-    //cvCopyMakeBorder(orig,dest,border,border,border,border,IPL_BORDER_CONSTANT);
     cvCopyMakeBorder(orig,dest,offset,IPL_BORDER_CONSTANT,cvScalar(0,0,0));
-
-		//cvShowImage("display",dest);
-		//cvWaitKey(10);
 		sensor_msgs::ImagePtr newmsg = sensor_msgs::CvBridge::cvToImgMsg(dest, "bgr8");
 		pub_.publish( newmsg );
+    cvReleaseImage(&dest);
   }
   else
     ROS_ERROR("Unable to convert %s image to bgr8", msg->encoding.c_str());
