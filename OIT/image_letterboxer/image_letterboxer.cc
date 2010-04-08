aab027f16776e1aa6233bebff05bfd0b121af678
Dave Feil-Seifer <dfseifer@usc.edu>
2010-04-08 00:29:06 +0000

    image_letterboxer: changed to use CameraSubscriber and CameraPublisher, now works with rviz and image_proc

--- OIT/image_letterboxer/image_letterboxer.cc
+++ OIT/image_letterboxer/image_letterboxer.cc
@@ -1,3 +1,37 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010. David Feil-Seifer
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
@@ -7,56 +41,13 @@
 #include <opencv/cxtypes.h>
 #include <image_transport/image_transport.h>
 
-
-image_transport::Subscriber sub_;
-image_transport::Publisher pub_;
-sensor_msgs::ImageConstPtr last_msg_;
+image_transport::CameraSubscriber sub_;
+image_transport::CameraPublisher pub_;
 sensor_msgs::CvBridge img_bridge_;
-
-ros::Publisher info_pub_;
-ros::Subscriber info_sub_;
-
-int old_width = 640;
-int old_height = 480;
 
 int border;
 
-int display = 0;
-
-void info_cb(const sensor_msgs::CameraInfoConstPtr& msg)
-{
-	sensor_msgs::CameraInfo newmsg = *msg;
-	newmsg.height += 2*border;
-	newmsg.width += 2*border;
-
-  
-
-  double fx = newmsg.P[0] ;//*(old_width+2*border) / old_width;
-  double fy = newmsg.P[5] ;//* (old_height+2*border) / old_height;
-
-  double cx = newmsg.P[2] + border;
-  double cy = newmsg.P[6] + border;
-
-	newmsg.P[2] = cx;
-	newmsg.P[6] = cy;
-  newmsg.K[2] = cx;
-  newmsg.K[5] = cy;
-
-	newmsg.P[0] = fx;
-	newmsg.P[5] = fy;
-  if( !display )
-  {
-    ROS_INFO( "height: %d width: %d", newmsg.width, newmsg.height );
-    for( int i = 0; i < 9; i++ )
-    {
-      ROS_INFO( "K[%d]: %f", i, newmsg.K[i]);
-    }
-    display = 1;
-  }
-	info_pub_.publish(newmsg);
-}
-
-void image_cb(const sensor_msgs::ImageConstPtr& msg)
+void image_cb(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& info_msg )
 {
   if (msg->encoding.find("bayer") != std::string::npos)
     boost::const_pointer_cast<sensor_msgs::Image>(msg)->encoding = "mono8";
@@ -65,12 +56,44 @@ void image_cb(const sensor_msgs::ImageConstPtr& msg)
   {
     IplImage *orig, *dest;
     orig = img_bridge_.toIpl();
+
+    // make new opencv image with border pixels around image */
+
 		CvPoint offset; offset.x = border; offset.y = border;
 		dest = cvCreateImage(cvSize(orig->width+border*2,orig->height+border*2), 8, 3 );
 		cvZero(dest);
     cvCopyMakeBorder(orig,dest,offset,IPL_BORDER_CONSTANT,cvScalar(0,0,0));
-		sensor_msgs::ImagePtr newmsg = sensor_msgs::CvBridge::cvToImgMsg(dest, "bgr8");
-		pub_.publish( newmsg );
+
+    // convert Ipl to sensor_msgs::Image
+		sensor_msgs::ImagePtr newimg = sensor_msgs::CvBridge::cvToImgMsg(dest, "bgr8");
+    newimg->header.frame_id = msg->header.frame_id;
+
+    // now change camera_info so that cx,cy is moved border,border pixels
+    sensor_msgs::CameraInfo newmsg;
+    newmsg = *info_msg;
+
+    newmsg.height += 2*border;
+  	newmsg.width += 2*border;
+
+    double fx = newmsg.P[0] ;
+    double fy = newmsg.P[5] ;
+
+    double cx = newmsg.P[2] + border;
+    double cy = newmsg.P[6] + border;
+
+	  newmsg.P[2] = cx;
+  	newmsg.P[6] = cy;
+    newmsg.K[2] = cx;
+    newmsg.K[5] = cy;
+
+	  newmsg.P[0] = fx;
+	  newmsg.P[5] = fy;
+
+
+    // publish letterboxed data
+		pub_.publish( *newimg, newmsg, info_msg->header.stamp );
+
+    // clean up
     cvReleaseImage(&dest);
   }
   else
@@ -82,12 +105,9 @@ int main( int argc, char* argv[] )
   ros::init(argc, argv, "image_letterboxer" );
   ros::NodeHandle nh;
   nh.param("border", border, 100 );
-	//cvNamedWindow("display",1);
   image_transport::ImageTransport it(nh);
-  sub_ = it.subscribe("image_raw", 1, &image_cb);
-  pub_ = it.advertise("letterbox/image_raw", 1);
-	info_sub_ = nh.subscribe( "camera_info", 1, &info_cb);
-	info_pub_ = nh.advertise<sensor_msgs::CameraInfo>( "letterbox/camera_info", 1 );
+  sub_ = it.subscribeCamera("image_raw", 1, &image_cb);
+  pub_ = it.advertiseCamera("letterbox/image_raw", 1);
   ros::spin();
 
   return 0;
