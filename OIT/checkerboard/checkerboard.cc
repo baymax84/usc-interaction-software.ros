5712a9d0fe2aab2df7af2180f22d3df2bdda1355
Dave Feil-Seifer <dfseifer@usc.edu>
2010-05-18 00:46:34 +0000

    * removed unecessary and commented code * forces frame_id and child_frame_id to be read from node parameters

--- OIT/checkerboard/checkerboard.cc
+++ OIT/checkerboard/checkerboard.cc
@@ -1,3 +1,25 @@
+/*
+ *  Checkerboard detector node for ROS
+ *  Copyright (C) 2010
+ *     David Feil-Seifer
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <image_geometry/pinhole_camera_model.h>
@@ -10,6 +32,8 @@
 sensor_msgs::CvBridge img_bridge;
 tf::TransformBroadcaster* tb;
 
+std::string camera_frame, checkerboard_frame;
+
 int nx = 8;
 int ny = 6;
 
@@ -17,9 +41,6 @@ std::vector<cv::Point3f> getObjPoints( int x, int y, double size )
 {
   bool xodd = x % 2 == 1; bool yodd = x % 2 == 1;
   std::vector<cv::Point3f> ret;
-  //cv::Mat ret(1, x*y, CV_32FC3 );
-  //cv::MatIterator_<cv::Point3f> ii = ret.begin<cv::Point3f>();
-  int ii = 0;
 
   for( int i = 0; i < y; i++ )
   {
@@ -32,8 +53,6 @@ std::vector<cv::Point3f> getObjPoints( int x, int y, double size )
       if( !xodd ) xcoord += size/2.0;
       cv::Point3f coord = cv::Point3f( xcoord, ycoord, 0);
       ret.push_back(coord);
-      //ret.at<cv::Point3f>(1,ii++) = coord;
-      //ii++;
     }
   }
   return ret;
@@ -57,17 +76,19 @@ void image_cb( const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraI
       
       image_geometry::PinholeCameraModel pcam;
       pcam.fromCameraInfo( infomsg);
-      //cv::Mat objpts(1,nx*ny, CV_32FC3);
       std::vector<cv::Point3f> objpts = getObjPoints(nx,ny,.1016);
 
       cv::solvePnP( objpts, corners, pcam.intrinsicMatrix(), pcam.distortionCoeffs(), rvec, tvec );
       
       tf::Transform checktf;
       checktf.setOrigin( tf::Vector3(tvec.at<double>(0,0),tvec.at<double>(0,1), tvec.at<double>(0,2) ) );
-      checktf.setRotation( tf::Quaternion(rvec.at<double>(0,2), rvec.at<double>(0,1), rvec.at<double>(0,0) ));
-      tb->sendTransform(tf::StampedTransform(checktf, msg->header.stamp, "camera", "checkerboard" ));
-      
-      //ROS_INFO( "tvec: %0.2f %0.2f %0.2f %0.2f rvec: %0.2f %0.2f %0.2f %0.2f", tvec.at<double>(0,0), tvec.at<double>(0,1), tvec.at<double>(0,2), tvec.at<double>(0,3), rvec.at<double>(0,0), rvec.at<double>(0,1), rvec.at<double>(0,2), rvec.at<double>(0,3));
+			tf::Quaternion quat;
+			quat.setEuler(-rvec.at<double>(0,2), -rvec.at<double>(0,1), -rvec.at<double>(0,0)-M_PI );
+      checktf.setRotation( quat );
+      tb->sendTransform(tf::StampedTransform(checktf, msg->header.stamp, camera_frame, checkerboard_frame ));
+      checktf.setOrigin( tf::Vector3(0,0, tvec.at<double>(0,2) ) );
+      //checktf.setRotation( tf::Quaternion( -rvec.at<double>(0,2), -rvec.at<double>(0,1),  -rvec.at<double>(0,0) ));
+      tb->sendTransform(tf::StampedTransform(checktf, msg->header.stamp, "/ovh_height", "/ovh" ));
     }
  }
   catch( sensor_msgs::CvBridgeException cvbe )
@@ -85,7 +106,8 @@ int main( int argc, char* argv[] )
   ros::init( argc, argv, "checkerboard" );
   ros::NodeHandle nh;
   tb = new tf::TransformBroadcaster();
-  //cvNamedWindow("corners", 1 );
+	nh.param("frame_id", camera_frame, std::string("camera") );
+	nh.param("child_frame_id", checkerboard_frame,std::string("checkerboard"));
   image_transport::ImageTransport it(nh);
   image_transport::CameraSubscriber image_sub = it.subscribeCamera("image", 1, image_cb );
 
