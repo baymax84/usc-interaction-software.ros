673f5997da2ca2c523af58b26c813c7f7329ceea
Edward T. Kaszubski <ekaszubski@gmail.com>
2010-06-23 00:22:04 +0000

    Added GPLv2 licensing to p2os_dashboard and p2os_teleop.

--- p2os/p2os_dashboard/src/p2os_dashboard/rosout_frame.py
+++ p2os/p2os_dashboard/src/p2os_dashboard/rosout_frame.py
@@ -30,6 +30,24 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+#       modifications to rosout_frame.py to work with p2os_dashboard
+#       Copyright (C) 2010  David Feil-Seifer [dfseifer@usc.edu], Edward T. Kaszubski [kaszubsk@usc.edu]
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
 import roslib
 roslib.load_manifest('p2os_dashboard')
 
