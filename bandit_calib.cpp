6742cd5973ef33ee514ec593fda93306ddb73aa5
Dave Feil-Seifer <dfseifer@usc.edu>
2010-06-26 00:19:59 +0000

    bandit: changed fakebandit to accomodate eyebrows

--- bandit_calib.cpp
+++ bandit_calib.cpp
@@ -19,12 +19,12 @@ std::vector<double> * joint_positions;
 
 double deg_to_rad(double deg)
 {
-	return deg * M_PI / 180;
+	return deg * M_PI / 180.0;
 }
 
 double rad_to_deg(double rad)
 {
-	return rad * 180 / M_PI;
+	return rad * 180.0 / M_PI;
 }
 
 void jointStatesCallBack(const sensor_msgs::JointStateConstPtr &js)
