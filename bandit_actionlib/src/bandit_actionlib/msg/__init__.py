b7ed1b4e367c9ce503fea622a3c178afd8ebca80
Dan Ho <daaango@gmail.com>
2010-08-02 23:47:44 +0000

    <empty message>

--- bandit_actionlib/src/bandit_actionlib/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from _BanditActionGoal import *
-from _BanditGoal import *
-from _BanditActionFeedback import *
-from _BanditFeedback import *
-from _BanditActionResult import *
-from _BanditAction import *
-from _BanditResult import *
