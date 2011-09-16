59c064b57b804a23c1c8cbf5e6dd4a899ed0e286
Edward T. Kaszubski <ekaszubski@gmail.com>
2011-09-16 00:37:28 +0000

    Added some generic cpp/ros tools like a ParamArrayLoader, a generic Container struct for dealing with arrays of types, and a GenericPolicyAdapter for working with policy classes.

---
+++ sandbox/usc_ros_tools/include/ros_tools/core.h
@@ -0,0 +1,6 @@
+#ifndef USC_ROS_TOOLS_ROS_TOOLS_CORE_H_
+#define USC_ROS_TOOLS_ROS_TOOLS_CORE_H_
+
+#include "param_array_reader.h"
+
+#endif // USC_ROS_TOOLS_ROS_TOOLS_CORE_H_
---
+++ sandbox/usc_ros_tools/src/core.cpp
@@ -0,0 +1,1 @@
+#include <ros_tools/core.h>
