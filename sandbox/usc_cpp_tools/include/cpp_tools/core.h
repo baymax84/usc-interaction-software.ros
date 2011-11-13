f64301d63f54363789742ecbfc9ed86fd915c22b
Edward T. Kaszubski <ekaszubski@gmail.com>
2011-11-13 23:32:41 +0000

    Removed usc_ros_tools and usc_cpp_tools; the functionality of both packages is covered in quickdev_cpp. Made cpp11 the default dependency for the -std=c++0x/11 flag.

--- sandbox/usc_cpp_tools/include/cpp_tools/core.h
+++
@@ -1,6 +0,0 @@
-#ifndef USC_CPP_TOOLS_CPP_TOOLS_CORE_H_
-#define USC_CPP_TOOLS_CPP_TOOLS_CORE_H_
-
-#include "generic_policy_adapter.h"
-
-#endif // USC_CPP_TOOLS_CPP_TOOLS_CORE_H_
--- sandbox/usc_cpp_tools/src/core.cpp
+++
@@ -1,1 +0,0 @@
-#include <cpp_tools/core.h>
