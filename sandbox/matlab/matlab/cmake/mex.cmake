fae867c7ed7741d871ff146c69c6ca1b6be05090
Edward T. Kaszubski <ekaszubski@gmail.com>
2011-11-15 18:43:47 +0000

    Updated how mex flags are imported; added include_mex() macro that should be used during initialization like find_package() but with no arguments. Removed unnecessary build files from matlab. Updated references.

--- sandbox/matlab/matlab/cmake/mex.cmake
+++ sandbox/matlab/matlab/cmake/mex.cmake
@@ -33,18 +33,41 @@
 #
 ###########################################################################
 
-set( MATLAB_DIR $ENV{MATLAB_DIR} ) #/usr/local/MATLAB/R2011a )
-set( OS $ENV{OS} ) #glnx )
-set( ARCH $ENV{ARCH} ) #a64 )
+macro( include_mex )
+	message( STATUS "----" )
+	message( STATUS "Reading MEX options..." )
 
-include_directories( ${MATLAB_DIR}/extern/include ${MATLAB_DIR}/simulink/include )
-link_directories( ${MATLAB_DIR}/bin/${OS}${ARCH} )
+	message( STATUS "Reading environment variables..." )
+	set( MATLAB_DIR $ENV{MATLAB_DIR} ) #/usr/local/MATLAB/R2011a )
+	message( STATUS "MATLAB_DIR set to ${MATLAB_DIR}" )
+	set( OS $ENV{OS} ) #glnx )
+	message( STATUS "OS set to ${OS}" )
+	set( ARCH $ENV{ARCH} ) #a64 )
+	message( STATUS "ARCH set to ${ARCH}" )
+	set( MEX_EXT $ENV{MEX_EXT} ) #a64
+	message( STATUS "MEX_EXT set to ${MEX_EXT}" )
+	message( STATUS "Done reading environment variables." )
+	#set( INCLUDE_SIMULINK $ENV{INCLUDE_SIMULINK} ) #0 )
 
-set( CMAKE_C_FLAGS ${CMAKE_C_FLAGS} -DMATLAB_MEX_FILE -ansi -D_GNU_SOURCE -fPIC -fno-omit-frame-pointer-pthread -DMX_COMPAT_32 -O -DNDEBUG )
-set( CMAKE_C_FLAGS ${CMAKE_C_FLAGS} -Wl,--version-script,${MATLAB_DIR}/extern/lib/${OS}${ARCH}/mexFunction.map -Wl,--no-undefined )
-set( CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -std=c++0x )
-
-set( MATLAB_LIBS mx mex mat m )
+	message( STATUS "Setting flags..." )
+	set( MEX_DEFINITIONS -DMATLAB_MEX_FILE -ansi -D_GNU_SOURCE -fPIC -fno-omit-frame-pointer -pthread -DMX_COMPAT_32 -O -DNDEBUG -Wl,--version-script,${MATLAB_DIR}/extern/lib/${OS}${ARCH}/mexFunction.map -Wl,--no-undefined -std=c++0x )
+	message( STATUS "MEX_DEFINITIONS set to ${MEX_DEFINITIONS}" )
+	set( MEX_LIBRARIES mx mex mat m )
+	message( STATUS "MEX_LIBRARIES set to ${MEX_LIBRARIES}" )
+	set( MEX_LIBRARY_DIRS ${MATLAB_DIR}/bin/${OS}${ARCH} )
+	message( STATUS "MEX_LIBRARY_DIRS set to ${MEX_LIBRARY_DIRS}" )
+	set( MEX_INCLUDE_DIRS ${MATLAB_DIR}/extern/include ${MATLAB_DIR}/simulink/include )
+	message( STATUS "MEX_INCLUDE_DIRS set to ${MEX_INCLUDE_DIRS}" )
+	message( STATUS "Done setting flags." )
+	message( STATUS "MEX options read." )
+	message( STATUS "----" )
+	message( STATUS "Importing MEX flags..." )
+	include_directories(${MEX_INCLUDE_DIRS})
+	link_directories(${MEX_LIBRARY_DIRS})
+	add_definitions(${MEX_DEFINITIONS})
+	message( STATUS "Done importing MEX flags." )
+	message( STATUS "----" )
+endmacro( include_mex )
 
 macro( rosbuild_add_mex lib )
   rosbuild_add_library( ${lib} ${ARGV} )
@@ -52,5 +75,5 @@ macro( rosbuild_add_mex lib )
   add_custom_command(
     TARGET ${lib}
     POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy ${LIBRARY_OUTPUT_PATH}/lib${lib}.so ${PROJECT_SOURCE_DIR}/mex/${lib}.mex${ARCH} )
+    COMMAND ${CMAKE_COMMAND} -E copy ${LIBRARY_OUTPUT_PATH}/lib${lib}.so ${PROJECT_SOURCE_DIR}/mex/${lib}.mex${MEX_EXT} )
 endmacro( rosbuild_add_mex )
