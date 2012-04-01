cecf1d2412b1bf0a8b4f5da32a5f35efd7e5dae3
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-04-01 06:38:09 +0000

    Fixed const qualifiers in math.h

--- quickdev/quickdev_cpp/include/quickdev/math.h
+++ quickdev/quickdev_cpp/include/quickdev/math.h
@@ -8,14 +8,14 @@ QUICKDEV_DECLARE_INTERNAL_NAMESPACE()
 // =============================================================================================================================================
 //! Get the sign of some value (result is { -1, 0, 1 }
 template<class __Data>
-inline __Data sign( __Data const & value ) const
+inline __Data sign( __Data const & value )
 {
     return value > 0 ? 1 : value < 0 ? -1 : 0;
 }
 
 //! Get the sign of some value (result is either -1 or 1)
 template<class __Data>
-inline __Data signNonzero( __Data const & value ) const
+inline __Data signNonzero( __Data const & value )
 {
     return value > 0 ? 1 : value < 0 ? -1 : 0;
 }
@@ -23,7 +23,7 @@ inline __Data signNonzero( __Data const & value ) const
 // =============================================================================================================================================
 //! Get the max of two values
 template<class __Data>
-inline __Data max( __Data const & value1, __Data const & value2 ) const
+inline __Data max( __Data const & value1, __Data const & value2 )
 {
     return std::max( value1, value2 );
 }
@@ -36,7 +36,7 @@ template
     class... __Args,
     typename std::enable_if<(sizeof...(__Args) > 1), int>::type = 0
 >
-inline __Data max( __Data const & value, __Args&&... args ) const
+inline __Data max( __Data const & value, __Args&&... args )
 {
     return std::max( value, max( std::forward<__Args>( args )... ) );
 }
@@ -44,7 +44,7 @@ inline __Data max( __Data const & value, __Args&&... args ) const
 // =============================================================================================================================================
 //! Get the min of two values
 template<class __Data>
-inline __Data min( __Data const & value1, __Data const & value2 ) const
+inline __Data min( __Data const & value1, __Data const & value2 )
 {
     return std::min( value1, value2 );
 }
@@ -57,7 +57,7 @@ template
     class... __Args,
     typename std::enable_if<(sizeof...(__Args) > 1), int>::type = 0
 >
-inline __Data min( __Data const & value, __Args&&... args ) const
+inline __Data min( __Data const & value, __Args&&... args )
 {
     return std::min( value, min( std::forward<__Args>( args )... ) );
 }
