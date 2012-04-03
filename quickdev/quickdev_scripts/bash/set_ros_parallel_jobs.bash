7b2a6b5d7a00311148a221c56b3b10989a57dce6
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-04-03 02:41:58 +0000

    Added -q flag to set_ros* scripts optionally keep output quiet.

--- quickdev/quickdev_scripts/bash/set_ros_parallel_jobs.bash
+++ quickdev/quickdev_scripts/bash/set_ros_parallel_jobs.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
+quiet_flag=0;
+
+while [ "$1" != "" ]; do
+  if [ $1 == "-q" ]; then quiet_flag=1; fi
+  shift
+done
+
 export ROS_PARALLEL_JOBS=-j`cat /proc/cpuinfo | grep -c processor`
 
-echo "ROS_PARALLEL_JOBS set to [" $ROS_PARALLEL_JOBS "]"
+if [ $quiet_flag != 1 ]; then echo "ROS_PARALLEL_JOBS set to [" $ROS_PARALLEL_JOBS "]"; fi
