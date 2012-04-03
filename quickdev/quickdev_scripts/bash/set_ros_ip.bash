df0b69db5daf0fd12d843f63b5562bcb926c8cc7
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-04-03 02:41:58 +0000

    Added -q flag to set_ros* scripts optionally keep output quiet.

--- quickdev/quickdev_scripts/bash/set_ros_ip.bash
+++ quickdev/quickdev_scripts/bash/set_ros_ip.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+quiet_flag=0;
+
+while [ "$1" != "" ]; do
+  if [ $1 == "-q" ]; then quiet_flag=1; fi
+  shift
+done
+
 export ROS_IP=`ip addr show | grep -m1 -oP "inet\s192\.\d+\.\d+\.\d+" | sed -e 's:inet\s::g'`
 
-echo "ROS_IP set to [" $ROS_IP "]"
+if [ $quiet_flag != 1 ]; then echo "ROS_IP set to [" $ROS_IP "]"; fi
