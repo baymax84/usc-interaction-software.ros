bcc76f9b1a33c0934562431b94d64ce5f0164b66
Ross Mead <qbitai@gmail.com>
2012-04-06 01:22:01 +0000

    Minor changes to sparky_node_new.cpp.

--- sparky/sparky_driver/src/sparky_node_new.cpp
+++ sparky/sparky_driver/src/sparky_node_new.cpp
@@ -6,9 +6,6 @@
 #include <sensor_msgs/JointState.h>
 #include <sparky/angle_servo_controller.h>
 //#include <sparky/controller.hh>
-#include <yaml-cpp/yaml.h>
-#include <fstream>
-#include <iostream>
 
 // angle (degrees and radians) definitions
 #define DTOR(rad) ((rad) * M_PI / 180.0)
@@ -80,17 +77,6 @@ int main(int argc, char** argv)
   // initialize ROS node
   ros::init(argc, argv, "sparky");
   ros::NodeHandle nh("~");
-
-  std::ifstream fin("params/params.yaml");
-  YAML::Parser parser(fin);
-  YAML::Node nodes;
-  parser.GetNextDocument(nodes);
-  std::string name;
-  for (unsigned int i = 0, n = nodes.size(); i < n; ++i)
-  {
-	  nodes[i]["name"] >> name;
-	  printf("%d: %s\n", i, name.c_str());
-  }
 
   // retrieve ports from parameter server
   //if (!nh.getParam("path", g_path)) nh.setParam("path", g_path);
