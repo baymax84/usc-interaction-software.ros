15b776df0e9107143c74a0d827626f489b26278c
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-05-10 23:23:05 +0000

    Added default params for pids and joints; used XmlRpcValues from the ROS parameter server instead of a YAML parser.

--- bandit_driver_prototype/include/bandit_driver/bandit_driver_node.h
+++ bandit_driver_prototype/include/bandit_driver/bandit_driver_node.h
@@ -38,35 +38,35 @@
 
 #include <quickdev/node.h>
 
+// policies
+#include <quickdev/service_server_policy.h>
+
 // objects
 #include <quickdev/multi_publisher.h>
 #include <quickdev/multi_subscriber.h>
 #include <bandit/bandit.h>
-//#include <urdf/model.h>
 #include <robot_state_publisher/robot_state_publisher.h>
 
-
-// messages
+// msgs
 #include <bandit_msgs/JointArray.h>
 #include <sensor_msgs/JointState.h>
 
+// srvs
+#include <bandit_msgs/GetJointProperties.h>
+
 // others
 #include <quickdev/geometry_message_conversions.h>
+#include <quickdev/numeric_unit_conversions.h>
 #include <kdl_parser/kdl_parser.hpp>
 
 typedef bandit_msgs::JointArray _BanditJointArrayMsg;
 typedef sensor_msgs::JointState _JointStateMsg;
 
-// Declare a node called BanditDriverNode.
-// A quickdev::RunablePolicy is automatically prepended to the list of policies our node will use.
-// To use more policies, simply list them here:
-//
-// QUICKDEV_DECLARE_NODE( BanditDriver, SomePolicy1, SomePolicy2 )
-//
-QUICKDEV_DECLARE_NODE( BanditDriver )
+typedef bandit_msgs::GetJointProperties _GetJointPropertiesSrv;
+typedef quickdev::ServiceServerPolicy<_GetJointPropertiesSrv> _GetJointPropertiesServiceServerPolicy;
 
-// Declare a class called BanditDriverNode
-//
+QUICKDEV_DECLARE_NODE( BanditDriver, _GetJointPropertiesServiceServerPolicy )
+
 QUICKDEV_DECLARE_NODE_CLASS( BanditDriver )
 {
     typedef robot_state_publisher::RobotStatePublisher _RobotStatePublisher;
@@ -78,67 +78,119 @@ QUICKDEV_DECLARE_NODE_CLASS( BanditDriver )
     KDL::Tree bandit_model_;
     boost::shared_ptr<_RobotStatePublisher> bandit_state_pub_ptr_;
 
-    // Variable initializations can be appended to this constructor as a comma-separated list:
-    //
-    // QUICKDEV_DECLARE_NODE_CONSTRUCTOR( BanditDriver ), member1_( some_value ), member2_( some_other_value ){}
-    //
-    QUICKDEV_DECLARE_NODE_CONSTRUCTOR( BanditDriver )
+    _GetJointPropertiesSrv::Response cached_joint_properties_;
+    bool bandit_state_initialized_;
+
+    QUICKDEV_DECLARE_NODE_CONSTRUCTOR( BanditDriver ),
+        bandit_state_initialized_( false )
     {
         //
     }
 
-    // This function is called by quickdev::RunablePolicy after all policies are constructed but just before the main loop is started.
-    // All policy initialization should be done here.
-    //
     QUICKDEV_SPIN_FIRST()
     {
-        // Say we had a policy called _SomePolicy that looked for the meta-parameter "some_value1_param" of type SomeType and
-        // "some_value2_param" of type SomeOtherType in its init function
-        // We can create those meta-params here and then pass them to all policies using initPolicies<...>():
-        //
-        // initPolicies<quickdev::policy::ALL>( "some_value1_param", SomeType(), "some_value2_param", SomeOtherType() );
-        //
-        // Or we can pass those meta-params only to _SomePolicy by specifying its type:
-        //
-        // initPolicies<_SomePolicy>( "some_value1_param", SomeType(), "some_value2_param", SomeOtherType() );
-        //
-        // If we want to initialize all policies and use their default values, we can simply call initPolicies<quickdev::policy::ALL>()
-        // with no arguments.
-        // Note that most initable policies won't function properly unless their init() functions are called directly or via initPolicies<...>().
-        // Furthermore, since each policy is required to track its initialization state, initPolicies<...>() is guaranteed to only call init()
-        // on policies that have yet to be initialized; therefore, calling initPolicies<quickdev::policy::ALL>() at the end of QUICKDEV_SPIN_FIRST()
-        // is always a safe operation.
-        // To instead force re-initialization, call forceInitPolicies<...>().
-        //
         QUICKDEV_GET_RUNABLE_NODEHANDLE( nh_rel );
 
+        // -------------------------------------------------------------------------------------------------------------------------------------
+        // load URDF and set up robot state publisher
         auto const urdf_filename = ros::ParamReader<std::string, 1>::readParam( nh_rel, "urdf_filename", "" );
-
-//        bandit_model_.initFile( urdf_filename );
 
         if( !kdl_parser::treeFromFile( urdf_filename, bandit_model_ ) )
         {
             ROS_ERROR( "Failed to construct kdl tree" );
-            QUICKDEV_GET_RUNABLE_POLICY()::interrupt();
+            return QUICKDEV_GET_RUNABLE_POLICY()::interrupt();
         }
 
         bandit_state_pub_ptr_ = quickdev::make_shared( new _RobotStatePublisher( bandit_model_ ) );
 
+        // -------------------------------------------------------------------------------------------------------------------------------------
+        // set up bandit driver object
         auto const port = ros::ParamReader<std::string, 1>::readParam( nh_rel, "port", "/dev/ttyUSB0" );
 
         bandit_driver_.openPort( port.c_str() );
-        bandit_driver_.registerStateCB( quickdev::auto_bind( &BanditDriverNode::banditStateCB, this ) );
 
-        multi_pub_.addPublishers<_JointStateMsg>( nh_rel, { "joint_state" } );
+        // load and parse joint config
+        auto & joints_map = bandit_driver_.getJoints();
 
-        multi_sub_.addSubscriber( nh_rel, "joint_command", &BanditDriverNode::jointCommandCB, this );
+        // read the full set of PID values for all joints off of the server at once
+        auto joints_pid_values = ros::ParamReader<XmlRpc::XmlRpcValue, 1>::readParam( nh_rel, "pids" );
+        // read the full set of config values for all joints off of the server at once
+        auto joints_config_values = ros::ParamReader<XmlRpc::XmlRpcValue, 1>::readParam( nh_rel, "joints" );
+
+        for( auto joints_it = joints_map.begin(); joints_it != joints_map.cend(); ++joints_it )
+        {
+            auto & joint = joints_it->second;
+
+            std::string joint_name = joint.name;
+
+            auto & joint_pid_values = joints_pid_values[joint_name];
+            auto & joint_config_values = joints_config_values[joint_name];
+
+            if( joint.type == smartservo::SMART_SERVO )
+            {
+                joint.setPIDConfig( (int) joint_pid_values["p"], (int) joint_pid_values["i"], (int) joint_pid_values["d"], (int) joint_pid_values["i_min"], (int) joint_pid_values["i_max"], (int) joint_pid_values["e_min"], (int) joint_pid_values["offset"] );
+            }
+
+            joint.setDirection( (int) joint_config_values["direction"] );
+            joint.setOffset( (double) joint_config_values["true_zero"] );
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------------------------
+        // set up params service server
+        _GetJointPropertiesServiceServerPolicy::registerCallback( quickdev::auto_bind( &BanditDriverNode::getJointPropertiesCB, this ) );
+        initPolicies<_GetJointPropertiesServiceServerPolicy>( "service_name_param", std::string( "get_joint_properties" ) );
+
+        auto const num_joints = bandit_driver_.getNumJoints();
+        cached_joint_properties_.id.reserve( num_joints );
+        cached_joint_properties_.name.reserve( num_joints );
+        cached_joint_properties_.min.reserve( num_joints );
+        cached_joint_properties_.max.reserve( num_joints );
+        cached_joint_properties_.pos.reserve( num_joints );
+
+        for( auto joints_it = joints_map.cbegin(); joints_it != joints_map.cend(); ++joints_it )
+        {
+            auto const & joint = joints_it->second;
+            cached_joint_properties_.id.push_back( joint.name );
+            cached_joint_properties_.name.push_back( joint.name );
+            cached_joint_properties_.min.push_back( Degree( Radian( joint.min ) ) );
+            cached_joint_properties_.max.push_back( Degree( Radian( joint.max ) ) );
+            cached_joint_properties_.pos.push_back( Degree( Radian( joint.getPos() ) ) );
+        }
+
+        // -------------------------------------------------------------------------------------------------------------------------------------
+        // create publishers and subscribers
+        multi_pub_.addPublishers<_JointStateMsg>( nh_rel, { "joint_states" } );
+
+        multi_sub_.addSubscriber( nh_rel, "joint_array_command", &BanditDriverNode::jointArrayCommandCB, this );
+        multi_sub_.addSubscriber( nh_rel, "joint_state_command", &BanditDriverNode::jointStateCommandCB, this );
 
         initPolicies<quickdev::policy::ALL>();
+
+        bool all_pid_configs_ok = false;
+        do
+        {
+            bandit_driver_.sendAllPIDConfigs();
+            bandit_driver_.processIO( 10000 );
+            bandit_driver_.processPackets();
+
+            all_pid_configs_ok = !bandit_driver_.checkAllPIDConfigs();
+
+            if( !all_pid_configs_ok ) ros::Duration( 1.0 ).sleep();
+        }
+        while( !all_pid_configs_ok );
+
+        for( auto joints_it = joints_map.begin(); joints_it != joints_map.cend(); ++joints_it )
+        {
+            auto & joint = joints_it->second;
+
+            joint.setPos( 0 );
+        }
+
+        bandit_driver_.sendAllJointPos();
+
+        bandit_driver_.registerStateCB( quickdev::auto_bind( &BanditDriverNode::banditStateCB, this ) );
     }
 
-    // This opitonal function is called by quickdev::RunablePolicy at a fixed rate (defined by the ROS param _loop_rate).
-    // Most updateable policies should have their update( ... ) functions called within this context.
-    //
     QUICKDEV_SPIN_ONCE()
     {
         //
@@ -146,59 +198,128 @@ QUICKDEV_DECLARE_NODE_CLASS( BanditDriver )
 
     void banditStateCB()
     {
+        bandit_state_initialized_ = true;
+
+        auto const now = ros::Time::now();
         // let's assume we're not going to change the number of joints or their name mappings after initialization
-        static const size_t num_joints = bandit_driver_.getNumJoints();
+        auto static const num_joints = bandit_driver_.getNumJoints();
         // lookup the eyebrow joint to prevent doing this for every joint every function call
-        static const size_t eyebrows_joint_index = bandit_driver_.getJointIndexByROSName( "eyebrows_joint" );
+        auto static const eyebrows_joint_index = bandit_driver_.getJointId( std::string( "eyebrows_joint" ) );
+
         _JointStateMsg joint_state_msg;
 
+        // reserve space in name and position to make push_back more efficient
         joint_state_msg.name.reserve( num_joints );
         joint_state_msg.position.reserve( num_joints );
 
-        joint_state_msg.header.stamp = ros::Time::now();
-        joint_state_msg.header.frame_id = "bandit_torso_link";
+        // set velocity and effort to 0
+        joint_state_msg.velocity.assign( num_joints, 0 );
+        joint_state_msg.effort.assign( num_joints, 0 );
 
-        std::map<std::string, double> joints_map;
+        joint_state_msg.header.stamp = now;
+        joint_state_msg.header.frame_id = "torso_link";
 
-        for ( size_t i = 0; i < num_joints; ++i )
+        std::map<std::string, double> joint_states_map;
+
+        auto const & joints_map = bandit_driver_.getJoints();
+
+        for( auto joints_it = joints_map.cbegin(); joints_it != joints_map.cend(); ++joints_it )
         {
-            auto const joint_name = bandit_driver_.getJointRosName( i );
-            auto const joint_pos = bandit_driver_.getJointPos( i );
+            auto const & joint = joints_it->second;
+
+            auto const joint_name = joint.name;
+            auto const joint_pos = joint.getPos();
             // the eyebrows are really two joints as far as robot_state_publisher is concerned
-            if ( i == eyebrows_joint_index )
+            if ( joint.name == eyebrows_joint_index )
             {
-                joint_state_msg.name.push_back( std::string( "bandit_head_left_brow_joint" ) );
-                joint_state_msg.position.push_back( -3 * joint_pos );
-                joint_state_msg.name.push_back( std::string( "bandit_head_right_brow_joint" ) );
-                joint_state_msg.position.push_back( -3 * joint_pos );
+                auto l_brow_pos = -3 * joint_pos;
+                auto r_brow_pos = -3 * joint_pos;
+
+                std::string l_brow_name( "head_left_brow_joint" );
+                std::string r_brow_name( "head_right_brow_joint" );
+
+                joint_state_msg.name.push_back( l_brow_name );
+                joint_state_msg.position.push_back( l_brow_pos );
+                joint_states_map[l_brow_name] = l_brow_pos;
+
+                joint_state_msg.name.push_back( r_brow_name );
+                joint_state_msg.position.push_back( r_brow_pos );
+                joint_states_map[r_brow_name] = r_brow_pos;
             }
             else
             {
                 joint_state_msg.name.push_back( joint_name );
                 joint_state_msg.position.push_back( joint_pos );
+                joint_states_map[joint_name] = joint_pos;
             }
-
-            joints_map[joint_name] = joint_pos;
         }
 
-        bandit_state_pub_ptr_->publishTransforms( joints_map, ros::Time::now() );
+        bandit_state_pub_ptr_->publishTransforms( joint_states_map, now );
 
-        multi_pub_.publish( "joint_state", quickdev::make_const_shared( joint_state_msg ) );
+        multi_pub_.publish( "joint_states", quickdev::make_const_shared( joint_state_msg ) );
     }
 
-    QUICKDEV_DECLARE_MESSAGE_CALLBACK( jointCommandCB, _BanditJointArrayMsg )
+    QUICKDEV_DECLARE_MESSAGE_CALLBACK( jointArrayCommandCB, _BanditJointArrayMsg )
     {
+        if( !bandit_state_initialized_ )
+        {
+            PRINT_WARN( "Ignoring joint commands; driver not yet initialized." );
+            return;
+        }
+
         auto const & joints = msg->joints;
 
         for( auto joints_it = joints.cbegin(); joints_it != joints.cend(); ++joints_it )
         {
-            auto const & joint_id = joints_it->id;
-            auto const & joint_angle = joints_it->angle;
-
-            bandit_driver_.setJointPos( joint_id, joint_angle );
+            bandit_driver_.setJointPos( joints_it->id, joints_it->angle );
         }
 
         bandit_driver_.sendAllJointPos();
+    }
+
+    QUICKDEV_DECLARE_MESSAGE_CALLBACK( jointStateCommandCB, _JointStateMsg )
+    {
+        if( !bandit_state_initialized_ )
+        {
+            PRINT_WARN( "Ignoring joint commands; driver not yet initialized." );
+            return;
+        }
+
+        auto const & names = msg->name;
+        auto const & positions = msg->position;
+
+        auto names_it = names.cbegin();
+        auto positions_it = positions.cbegin();
+
+        for( ; names_it != names.cend(); ++names_it, ++positions_it )
+        {
+            bandit_driver_.setJointPos( *names_it, *positions_it );
+        }
+
+        bandit_driver_.sendAllJointPos();
+    }
+
+    QUICKDEV_DECLARE_SERVICE_CALLBACK( getJointPropertiesCB, _GetJointPropertiesSrv )
+    {
+        if( !bandit_state_initialized_ )
+        {
+            PRINT_WARN( "Ignoring GetJointProperties request; driver not yet initialized." );
+            return false;
+        }
+
+        auto const & joints_map = bandit_driver_.getJoints();
+
+        cached_joint_properties_.pos.clear();
+        cached_joint_properties_.pos.reserve( joints_map.size() );
+
+        for( auto joints_it = joints_map.cbegin(); joints_it != joints_map.cend(); ++joints_it )
+        {
+            cached_joint_properties_.pos.push_back( Degree( Radian( joints_it->second.getPos() ) ) );
+        }
+
+        response = cached_joint_properties_;
+
+        return true;
     }
 };
 
