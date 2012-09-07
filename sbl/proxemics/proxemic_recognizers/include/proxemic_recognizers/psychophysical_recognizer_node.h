7346aa1760f7b5871ee30a095b242d5995803ec9
Edward T. Kaszubski <ekaszubski@gmail.com>
2012-09-06 21:23:57 -0700

    Renamed sbl_experimental to sbl. Cleaned up various packages within sbl.

---
+++ sbl/proxemics/proxemic_recognizers/include/proxemic_recognizers/psychophysical_recognizer_node.h
@@ -0,0 +1,117 @@
+/***************************************************************************
+ *  include/proxemic_recognizers/psychophysical_recognizer_node.h
+ *  --------------------
+ *
+ *  Copyright (c) 2011, Edward T. Kaszubski ( ekaszubski@gmail.com )
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are
+ *  met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following disclaimer
+ *    in the documentation and/or other materials provided with the
+ *    distribution.
+ *  * Neither the name of usc-ros-pkg nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SPSYCHOPHYSICAL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **************************************************************************/
+
+#ifndef PROXEMICRECOGNIZERS_PSYCHOPHYSICALRECOGNIZER_H_
+#define PROXEMICRECOGNIZERS_PSYCHOPHYSICALRECOGNIZER_H_
+
+#include <quickdev/node.h>
+
+#include <proxemic_models/psychophysical_features.h>
+
+#include <humanoid_recognizers/humanoid_recognizer_policy.h>
+
+using humanoid::_HumanoidStateMsg;
+using proxemics::_PsychophysicalFeatureMsg;
+using proxemics::_PsychophysicalFeatureArrayMsg;
+
+typedef HumanoidRecognizerPolicy<_PsychophysicalFeatureArrayMsg> _HumanoidRecognizerPolicy;
+
+QUICKDEV_DECLARE_NODE( PsychophysicalRecognizer, _HumanoidRecognizerPolicy )
+
+QUICKDEV_DECLARE_NODE_CLASS( PsychophysicalRecognizer )
+{
+    _MarkerMsg marker_template_;
+    QUICKDEV_DECLARE_NODE_CONSTRUCTOR( PsychophysicalRecognizer )
+    {
+        //
+    }
+
+    QUICKDEV_SPIN_FIRST()
+    {
+        initPolicies<_HumanoidRecognizerPolicy>( "update_pairs_param", true );
+        initPolicies<quickdev::policy::ALL>();
+
+        marker_template_.header.frame_id = "/world";
+        marker_template_.ns = "mehrabian_visualization";
+        marker_template_.action = visualization_msgs::Marker::ADD;
+        marker_template_.lifetime = ros::Duration( 0.1 );
+        marker_template_.pose.orientation.w = 1.0;
+    }
+
+    QUICKDEV_SPIN_ONCE()
+    {
+        auto const now = ros::Time::now();
+
+        QUICKDEV_GET_RUNABLE_NODEHANDLE( nh_rel );
+
+        _MarkerArrayMsg markers_msg;
+        _PsychophysicalFeatureArrayMsg features_msg;
+
+        unsigned int current_id = 0;
+
+        auto const & humanoid_pairs = _HumanoidRecognizerPolicy::getHumanoidPairs();
+
+        for( auto pair = humanoid_pairs.cbegin(); pair != humanoid_pairs.cend(); ++pair )
+        {
+            auto feature1( proxemics::PsychophysicalFeatureRecognizer( pair->first, nh_rel ) );
+            auto feature2( proxemics::PsychophysicalFeatureRecognizer( pair->second, nh_rel ) );
+
+            features_msg.features.push_back( feature1.createMessage( feature2 ) );
+            features_msg.features.push_back( feature2.createMessage( feature1 ) );
+
+            //const auto & joint1 = _HumanoidRecognizerPolicy::states_map_[pair->first.name]["torso"];
+            //const auto & joint2 = _HumanoidRecognizerPolicy::states_map_[pair->second.name]["torso"];
+
+            /*std_msgs::ColorRGBA current_color;
+            current_color.r = 0.0;
+            current_color.g = 0.0;
+            current_color.b = 1.0;
+            current_color.a = 1.0;
+
+            _MarkerMsg arrow_marker = arrow_marker_template_;
+            arrow_marker.header.stamp = now;
+            arrow_marker.id = current_id ++;
+            arrow_marker.color = current_color;
+            arrow_marker.points.push_back( joint1.pose.pose.position );
+            arrow_marker.points.push_back( joint2.pose.pose.position );
+
+            markers_msg.markers.push_back( arrow_marker );*/
+        }
+
+        _HumanoidRecognizerPolicy::updateFeatures( features_msg );
+    }
+};
+
+#endif // PROXEMICRECOGNIZERS_PSYCHOPHYSICALRECOGNIZER_H_
